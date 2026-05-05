8423b778c67fa0e1f5aa5c0682967f4549e6c806
srimoy-stack <das6370773567@gmail.com>
2026-05-05 19:29:55 +0530

    fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,6 +5,7 @@ const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': 'true',
     },
 });
 
